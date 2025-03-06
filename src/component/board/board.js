da91332fb7c8f7a1739d896f18360f84bf5ee18a
James <26018087+Jameschan215@user.noreply.github.com>
2025-03-06 18:51:30 +0800

    Add new features
    When placing ships, users can click on them to
    change their direction.

--- src/component/board/board.js
+++ src/component/board/board.js
@@ -2,10 +2,11 @@ import './board.css';
 import { range } from '../../script/utils.js';
 import { ComputerPlayer, HumanPlayer } from '../../script/player.js';
 import { ShipComponent } from '../ship-component/ship-component.js';
+import { CELL_SIZE } from '../../script/constants.js';
 
 export function Board(game, player) {
 	const { isGameRunning, isGameOver, currentPlayer, handleClick } = game;
-	const board = player.gameboard.board;
+	const gameboard = player.gameboard;
 
 	const cellContent = {
 		hit: 'H',
@@ -23,6 +24,28 @@ export function Board(game, player) {
 		.map((letter) => `<div class="label">${letter}</div>`)
 		.join('');
 
+	// Shake animation
+	const shakeElement = (element, duration = 200, shakeDistance = 5) => {
+		element.style.transition = `transform ${duration / 4}ms ease-in-out`;
+		element.style.transform = `translateX(${shakeDistance}px)`;
+		element.style.borderColor = 'red';
+
+		setTimeout(() => {
+			element.style.transform = `translateX(-${shakeDistance}px)`;
+			setTimeout(() => {
+				element.style.transform = `translateX(${shakeDistance / 2}px)`;
+				setTimeout(() => {
+					element.style.transform = `translateX(-${shakeDistance / 2}px)`;
+					setTimeout(() => {
+						element.style.transform = `translateX(0)`;
+						element.style.transition = ''; // Remove transition for future changes if needed.
+						element.style.borderColor = 'green'; // Remove transition for future changes if needed.
+					}, duration / 4);
+				}, duration / 4);
+			}, duration / 4);
+		}, duration / 4);
+	};
+
 	// Cell event handler
 	const clickHandler = (event) => {
 		// Left-click only
@@ -36,6 +59,53 @@ export function Board(game, player) {
 			console.log(row, col);
 			handleClick(row, col);
 		}
+	};
+
+	// Rotate ship handler
+	const switchDirection = (direction) =>
+		direction === 'horizontal' ? 'vertical' : 'horizontal';
+
+	const rotateShipHandler = (event) => {
+		const size = Number(event.target.dataset.size);
+		const direction = event.target.dataset.direction;
+		const startRow = Number(event.target.dataset.startRow);
+		const startCol = Number(event.target.dataset.startCol);
+		const index = Number(event.target.id.split('-')[1]);
+
+		const originalShips = gameboard.ships;
+		gameboard.ships = gameboard.ships.filter((_, i) => i !== index);
+		const { success } = gameboard.placeShip(
+			size,
+			startRow,
+			startCol,
+			switchDirection(direction)
+		);
+
+		if (!success) {
+			gameboard.ships = originalShips;
+			shakeElement(event.target);
+		} else {
+			game.updateUI();
+		}
+	};
+
+	const dropHandler = (event) => {
+		event.preventDefault();
+		const draggedId = event.dataTransfer.getData('text/plain');
+		const draggedIndex = Number(draggedId.split('-')[1]);
+		const [endRow, endCol] = event.target.dataset.coordinate
+			.split(',')
+			.map(Number);
+		const draggedEle = document.getElementById(draggedId);
+		const direction = draggedEle.dataset.direction;
+		const size = Number(draggedEle.dataset.size);
+
+		const originalShips = gameboard.ships;
+		gameboard.ships = gameboard.ships.filter((_, i) => i !== draggedIndex);
+		const { success } = gameboard.placeShip(size, endRow, endCol, direction);
+		if (!success) gameboard.ships = originalShips;
+
+		game.updateUI();
 	};
 
 	const html = document.createElement('div');
@@ -64,21 +134,24 @@ export function Board(game, player) {
 		boardDom.addEventListener('click', clickHandler);
 	}
 	// Append cells
-	board.forEach((row, i) => {
+	gameboard.board.forEach((row, i) => {
 		row.forEach((cell, j) => {
 			const cellDom = document.createElement('div');
 			cellDom.className = `cell ${cell.state}`;
 			cellDom.dataset.coordinate = `${i},${j}`;
 			cellDom.innerHTML = cellContent[cell.state];
-
+			cellDom.addEventListener('dragover', (e) => e.preventDefault());
+			cellDom.addEventListener('drop', dropHandler);
 			boardDom.appendChild(cellDom);
 		});
 	});
 
+	// Place ships on board
 	const ships = document.createElement('div');
 	ships.className = 'ships';
-	player.gameboard.ships.forEach(({ ship, positions }) => {
-		const shipComponent = ShipComponent(ship, positions);
+	gameboard.ships.forEach(({ ship, positions }, index) => {
+		const shipComponent = ShipComponent(ship, positions, index);
+		shipComponent.addEventListener('click', rotateShipHandler);
 		ships.appendChild(shipComponent);
 	});
 
--- src/component/ship-component/ship-component.js
+++ src/component/ship-component/ship-component.js
@@ -2,7 +2,7 @@ import './ship-component.css';
 
 import { CELL_SIZE } from '../../script/constants.js';
 
-export function ShipComponent(ship, positions) {
+export function ShipComponent(ship, positions, index) {
 	const [startRow, startCol] = positions[0];
 
 	const shipComponent = document.createElement('div');
@@ -10,6 +10,20 @@ export function ShipComponent(ship, positions) {
 		ship.direction === 'horizontal'
 			? `ship ship-h h-${ship.size}`
 			: `ship ship-v v-${ship.size}`;
+
+	shipComponent.dataset.direction = ship.direction;
+	shipComponent.dataset.size = ship.size;
+	shipComponent.dataset.startRow = startRow;
+	shipComponent.dataset.startCol = startCol;
+
+	shipComponent.id = `ship-${index}`;
+	shipComponent.draggable = true;
+	shipComponent.addEventListener('dragstart', (event) => {
+		const startRow = event.target.dataset.startRow;
+		const startCol = event.target.dataset.startCol;
+		event.dataTransfer.setData('text/plain', event.target.id);
+		console.log({ startRow, startCol });
+	});
 
 	if (ship.isSunk()) {
 		shipComponent.classList.add('sunk');
