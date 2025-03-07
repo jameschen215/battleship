8e4a72095c2843fd4c3bbebae11ba32faffec916
James <26018087+Jameschan215@user.noreply.github.com>
2025-03-07 10:16:51 +0800

    Let users place ships manually

--- src/component/board/board.js
+++ src/component/board/board.js
@@ -1,8 +1,7 @@
 import './board.css';
 import { range } from '../../script/utils.js';
 import { ComputerPlayer, HumanPlayer } from '../../script/player.js';
-import { ShipComponent } from '../ship-component/ship-component.js';
-import { CELL_SIZE } from '../../script/constants.js';
+import { Ship } from '../ship/ship.js';
 
 export function Board(game, player) {
 	const { isGameRunning, isGameOver, currentPlayer, handleClick } = game;
@@ -62,50 +61,91 @@ export function Board(game, player) {
 	};
 
 	// Rotate ship handler
-	const switchDirection = (direction) =>
-		direction === 'horizontal' ? 'vertical' : 'horizontal';
-
 	const rotateShipHandler = (event) => {
-		const size = Number(event.target.dataset.size);
-		const direction = event.target.dataset.direction;
-		const startRow = Number(event.target.dataset.startRow);
-		const startCol = Number(event.target.dataset.startCol);
-		const index = Number(event.target.id.split('-')[1]);
+		// Prevent click from firing during drag - Ignore if dragged or not left-click
+		if (event.defaultPrevented || event.button !== 0) return;
 
+		const shipDom = event.currentTarget;
+		const shipDirection = shipDom.dataset.direction;
+		const shipIndex = Number(shipDom.dataset.index);
+		const shipSize = Number(shipDom.dataset.size);
+		const [startRow, startCol] = shipDom.dataset.startCoordinate
+			.split(',')
+			.map(Number);
+
+		// Rotate ships:
+		// 1. Remove the ship from the array of ships.
 		const originalShips = gameboard.ships;
-		gameboard.ships = gameboard.ships.filter((_, i) => i !== index);
+		gameboard.ships = gameboard.ships.filter((_, i) => i !== shipIndex);
+
+		// 2. Place the ship on board again with a new direction.
 		const { success } = gameboard.placeShip(
-			size,
+			shipSize,
 			startRow,
 			startCol,
-			switchDirection(direction)
+			shipDirection === 'horizontal' ? 'vertical' : 'horizontal'
 		);
 
+		// 3. If the placement fails,
+		// revert the array of ships to its original state, and shake.
 		if (!success) {
 			gameboard.ships = originalShips;
-			shakeElement(event.target);
+			shakeElement(event.currentTarget);
 		} else {
 			game.updateUI();
 		}
 	};
 
+	// Drop handler
 	const dropHandler = (event) => {
 		event.preventDefault();
-		const draggedId = event.dataTransfer.getData('text/plain');
-		const draggedIndex = Number(draggedId.split('-')[1]);
-		const [endRow, endCol] = event.target.dataset.coordinate
-			.split(',')
-			.map(Number);
-		const draggedEle = document.getElementById(draggedId);
-		const direction = draggedEle.dataset.direction;
-		const size = Number(draggedEle.dataset.size);
 
-		const originalShips = gameboard.ships;
-		gameboard.ships = gameboard.ships.filter((_, i) => i !== draggedIndex);
-		const { success } = gameboard.placeShip(size, endRow, endCol, direction);
-		if (!success) gameboard.ships = originalShips;
+		const shipId = event.dataTransfer.getData('text/plain');
+		const shipDom = document.getElementById(shipId);
+		const shipDirection = shipDom.dataset.direction;
+		const shipIndex = Number(shipDom.dataset.index);
+		const shipSize = Number(shipDom.dataset.size);
+		const componentIndex = Number(
+			event.dataTransfer.getData('shipComponentIndex')
+		);
 
-		game.updateUI();
+		const dropX = event.clientX;
+		const dropY = event.clientY;
+		const cell = document.elementFromPoint(dropX, dropY).closest('div.cell');
+
+		if (cell) {
+			let endRow = null;
+			let endCol = null;
+
+			const [cellRow, cellCol] = cell.dataset.coordinate.split(',').map(Number);
+
+			if (shipDom.dataset.direction === 'horizontal') {
+				endRow = cellRow;
+				endCol = cellCol - componentIndex;
+			} else {
+				endRow = cellRow - componentIndex;
+				endCol = cellCol;
+			}
+
+			// Move ships:
+			// 1. Remove the ship from the array of ships.
+			const originalShips = gameboard.ships;
+			gameboard.ships = gameboard.ships.filter((_, i) => i !== shipIndex);
+
+			// 2. Place the ship on board again with a new coordinate.
+			const { success } = gameboard.placeShip(
+				shipSize,
+				endRow,
+				endCol,
+				shipDirection
+			);
+
+			// 3. If the placement fails,
+			// revert the array of ships to its original state.
+			if (!success) gameboard.ships = originalShips;
+
+			game.updateUI();
+		}
 	};
 
 	const html = document.createElement('div');
@@ -150,9 +190,9 @@ export function Board(game, player) {
 	const ships = document.createElement('div');
 	ships.className = 'ships';
 	gameboard.ships.forEach(({ ship, positions }, index) => {
-		const shipComponent = ShipComponent(ship, positions, index);
-		shipComponent.addEventListener('click', rotateShipHandler);
-		ships.appendChild(shipComponent);
+		const shipDom = Ship(ship, positions, index);
+		shipDom.addEventListener('click', rotateShipHandler);
+		ships.appendChild(shipDom);
 	});
 
 	boardDom.appendChild(ships);
---
+++ src/component/ship/ship.js
@@ -0,0 +1,61 @@
+import './ship.css';
+
+import { CELL_SIZE } from '../../script/constants.js';
+
+export function Ship(ship, positions, index) {
+	const [startRow, startCol] = positions[0];
+
+	const shipDom = document.createElement('div');
+	shipDom.id = `ship-${index}`;
+	shipDom.className = `ship ship-${ship.size} ${ship.direction}`;
+	shipDom.dataset.index = index;
+	shipDom.dataset.size = ship.size;
+	shipDom.dataset.direction = ship.direction;
+	shipDom.dataset.startCoordinate = `${startRow},${startCol}`;
+	shipDom.draggable = true;
+
+	shipDom.style.top = startRow * CELL_SIZE + 1 + 'px';
+	shipDom.style.left = startCol * CELL_SIZE + 1 + 'px';
+
+	let clickedComponent = null;
+	shipDom.addEventListener('mousedown', (event) => {
+		clickedComponent = event.target.closest('div.ship-component');
+	});
+
+	shipDom.addEventListener('dragstart', (event) => {
+		if (clickedComponent) {
+			event.dataTransfer.setData('text/plain', shipDom.id);
+			event.dataTransfer.setData(
+				'shipComponentIndex',
+				clickedComponent.dataset.index
+			);
+		}
+
+		shipDom.addEventListener('drag', (event) => {
+			shipDom.style.pointerEvents = 'none';
+		});
+
+		shipDom.addEventListener('dragend', (event) => {
+			shipDom.style.pointerEvents = 'auto';
+		});
+	});
+
+	if (ship.isSunk()) {
+		shipDom.classList.add('sunk');
+	}
+
+	// Add ship components
+	positions.forEach((_, index) => {
+		shipDom.appendChild(ShipComponent(index));
+	});
+
+	return shipDom;
+}
+
+function ShipComponent(index) {
+	const shipComponent = document.createElement('div');
+	shipComponent.className = 'ship-component';
+	shipComponent.dataset.index = index;
+
+	return shipComponent;
+}
